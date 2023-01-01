2fd6ddd23ec3d0a2ea275e8ce4aae118f4c48ff0
ansbaby7 <ansbaby7@gmail.com>
2023-01-01 23:05:14 +0530

    Complete catr

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{error::Error, io::{BufRead, BufReader, self}, fs::File};
 use clap::{Command, Arg, ArgAction};
 
 #[derive(Debug)]
@@ -6,6 +6,13 @@ pub struct Config {
     files: Vec<String>,
     number_lines: bool,
     number_nonblank_lines: bool,
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
 
 pub fn get_args() -> MyResult<Config> {
@@ -49,7 +56,30 @@ pub fn get_args() -> MyResult<Config> {
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 pub fn run(config: Config) -> MyResult<()> {
-    dbg!(config);
+    let mut line_num = 0;
+    for filename in config.files {
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open {}: {}", filename, err),
+            Ok(file) => {
+                for  line in file.lines() {
+                    let line = line?;
+                    if config.number_lines{
+                        line_num += 1;
+                        println!("{:>6}\t{}",line_num, line);
+                    } else if config.number_nonblank_lines {
+                        if !line.is_empty() {
+                            line_num += 1;
+                            println!("{:>6}\t{}",line_num, line);                            
+                        } else {
+                            println!();
+                        }
+                    } else {
+                        println!("{}", line);
+                    }
+                }
+            }
+        }
+    }
     Ok(())
 }
 
